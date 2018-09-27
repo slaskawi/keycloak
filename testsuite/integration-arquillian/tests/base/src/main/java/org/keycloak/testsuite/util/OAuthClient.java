33b508ccfe5d009b64cd7eebf2627d95c28d7d79
Sebastian Laskawiec <slaskawi@redhat.com>
2018-09-27 11:24:33 +0200

    KEYCLOAK-8349 Use TLS for all tests in the suite

--- testsuite/integration-arquillian/tests/base/src/main/java/org/keycloak/testsuite/util/OAuthClient.java
+++ testsuite/integration-arquillian/tests/base/src/main/java/org/keycloak/testsuite/util/OAuthClient.java
@@ -90,6 +90,7 @@ public class OAuthClient {
     public static String SERVER_ROOT;
     public static String AUTH_SERVER_ROOT;
     public static String APP_ROOT;
+    public static String APP_AUTH_ROOT;
     private static final boolean sslRequired = Boolean.parseBoolean(System.getProperty("auth.server.ssl.required"));
 
     static {
@@ -101,6 +102,7 @@ public class OAuthClient {
         SERVER_ROOT = serverRoot;
         AUTH_SERVER_ROOT = SERVER_ROOT + "/auth";
         APP_ROOT = AUTH_SERVER_ROOT + "/realms/master/app";
+        APP_AUTH_ROOT = APP_ROOT + "/auth";
     }
 
     private WebDriver driver;
